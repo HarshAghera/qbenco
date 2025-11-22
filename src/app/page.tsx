76f1d7fe61ef6143420f190cf92e17bb811b31f4
Harsh Aghera <harsh.aghera2@gmail.com>
2025-11-22 17:07:10 +0530

    Fixed CI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import React from 'react'
-
 const Home = () => {
   return (
-    <section className="py-24">
-      <div className="container">
-        <h1 className="text-5xl font-bold">
-          Meet
-        </h1>
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className='text-5xl font-bold'>Meet</h1>
       </div>
     </section>
   )
 }
 
-export default Home;
+export default Home
